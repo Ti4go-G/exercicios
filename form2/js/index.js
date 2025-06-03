dfb2e733e74b2e8decbf00338ab59b27eddb7fb9
Ti4go-G <tiago.g2511@hotmail.com>
2025-06-03 16:18:48 -0300

    feat: logica de validação inicial
    treinando validação de form usando classes

---
+++ form2/js/index.js
@@ -0,0 +1,68 @@
+
+class ValidaForm {
+    constructor() {
+        this.form = document.querySelector('.form')
+        this.events();
+    }
+    events() {
+        this.form.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        })
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const validFields = this.checkFields();
+        const validPassword = this.checkPassword();
+    }
+    checkPassword() {
+        const password = document.getElementById('password')
+        const passwordRepeat = document.getElementById('password-repeat')
+        isValid = true;
+        if(password.value.length < 6 || password.value.length > 12){
+            this.createError(password, `A senha precisa ter entre 6 e 12 caracteres`)
+            isValid = false
+        }
+    }
+    checkFields() {
+        let isValid = true;
+        for (let error of this.form.querySelectorAll('.error-text')) {
+            error.remove();
+        }
+        for (let field of this.form.querySelectorAll('input')) {
+            const label = field.previousElementSibling.innerText
+            if (!field.value) {
+                this.createError(field, `O campo ${label} não pode estar em branco`);
+                isValid = false;
+            }
+            if (field.id === 'cpf') {
+
+            }
+            if (field.id === 'user' && !this.validateUser(field)) {
+                isValid = false;
+            }
+        }
+    }
+    validateUser(field) {
+        const user = field.value;
+        let isValid = true;
+
+        if (user.length < 3 || user.length > 12) {
+            this.createError(field, `O usuário precisa ter entre 3 e 12 caracteres`)
+            isValid = false;
+        }
+        if (!user.match(/^[a-zA-Z0-9]+$/g)) {
+            this.criaErro(field, 'Nome de usuário precisar conter apenas letras e/ou números.');
+            isValid = false;
+        }
+        return isValid
+    }
+    createError(field, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg
+        div.classList.add('error-text')
+        field.insertAdjacentElement('afterend', div)
+    }
+
+}
+
+const valida = new ValidaForm();
