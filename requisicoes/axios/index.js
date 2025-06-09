4d02c5821a2bd01c7c420fcbd9c7a456d96a2038
Ti4go-G <tiago.g2511@hotmail.com>
2025-06-09 20:58:05 -0300

    add do patch, put e delete

--- requisicoes/axios/index.js
+++ requisicoes/axios/index.js
@@ -3,9 +3,12 @@
 */
 import axios from 'axios'
 
-async function criarPost(post) {
+async function criarPost({title, body}) {
     try {
-        const response = await axios.post('https://jsonplaceholder.typicode.com/posts')
+        const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+            title: title,
+            body: body
+        })
         console.log('post criado com sucesso', response.data)
     } catch (error) {
         console.error('erro ao criar post:', error)
@@ -23,9 +26,37 @@ async function getPosts() {
 getPosts()
 async function updatePostTitle(id, title) {
     try {
-        
+        const response = await axios.patch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            title: title
+        })
+        console.log('titulo alterado com sucesso, novo titulo: ', response.data)
     } catch (error) {
-        
+        console.error('erro ao atualizar titulo do post', error)
     }
     
+}
+async function updatePost({userId, id, title, body}) {
+    try{
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            userId: userId,
+            id: id, 
+            title: title,
+            body: body
+        })
+        console.log('post atualizado com sucesso, novo post: ', response.data)
+    }catch{
+        console.error('erro ao atualizar o post: ', error)
+    }
+}
+
+async function deletePost(id) {
+    try {
+        const response = await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        console.log('post deletado com sucesso')
+        if(response.statusText.toLowerCase()!== 'ok' ){
+            throw new Error(`erro ao deletar post, código: ${response.status}`)
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
 }
