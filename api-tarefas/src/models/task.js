2f5a9a180dc3e1f857011f4520d5b8838af2b316
Ti4go-G <tiago.g2511@hotmail.com>
2025-08-14 18:24:29 -0300

    feat: criando model de tasks

--- api-tarefas/src/models/task.js
+++ api-tarefas/src/models/task.js
@@ -1,2 +1,16 @@
-let tasks = [];
-module.exports = tasks
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
